43d936acaa1555971e3842b1bee6a1dc724b2630
arefin.aareef <arefin.aareef@gmail.com>
2023-10-15 18:08:23 +0600

    action section added

--- components/action.tsx
+++ components/action.tsx
@@ -1,18 +1,18 @@
-import { Flex, Heading, Text, Container } from "@chakra-ui/react";
-
+import { Flex, Heading, Text, Button } from "@chakra-ui/react";
 
 const Action = () => {
-    return (
-        <Flex>
-            <Heading fontSize={40} fontWeight='bold' mx='auto'>
-                Start your 14 days free trial
-            </Heading>
-            <Text fontSize={14} color='#737373' fontWeight='bold' mx='auto'>
-            Met minim Mollie non desert Alamo est sit cliquey dolor 
-do met sent. RELIT official consequent.
-            </Text>
-        </Flex>
-    );
+  return (
+    <Flex direction="column" my={10}>
+      <Heading fontSize={40} fontWeight="bold" mx="auto">
+        Start your 14 days free trial
+      </Heading>
+      <Text textAlign='center' fontSize={14} my={30} color="#737373" fontWeight="bold" mx="auto">
+        Met minim Mollie non desert Alamo est sit cliquey dolor <br/> do met sent.
+        RELIT official consequent.
+      </Text>
+      <Button color='#ffffff' backgroundColor='#23A6F0' size='lg' mx={'auto'}>Try it for free</Button>
+    </Flex>
+  );
 };
 
 export default Action;
